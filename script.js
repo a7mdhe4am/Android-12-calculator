b56966500cf62b567b8c3eb3c40c957ddb426dd8
a7mdhe4am <90359244+a7mdhe4am@users.noreply.github.com>
2022-08-31 03:38:50 +0200

    commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const numberBtns = document.querySelectorAll("[data-number]")
+const operandBtns = document.querySelectorAll("[data-operand]")
+const clearBtn = document.querySelector("[data-clear]")
+const deleteBtn = document.querySelector("[data-delete]")
+const equalsBtn = document.querySelector("[data-equals]")
+const lastOperation = document.querySelector("[data-last-result-screen]")
+const currentOperation = document.querySelector("[data-current-result-screen]")
+let firstNumber = ''
+let secondNumber = ''
+
+function add(x, y) {
+    
+}
+
+
